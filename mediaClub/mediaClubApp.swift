710178456ea90b6aa8af803c20fe3aee66b59b9c
Asmaa Eliwa <asmaagamalelsayed96@gmail.com>
2023-11-28 07:56:31 -0400

    setting app

---
+++ mediaClub/mediaClubApp.swift
@@ -0,0 +1,20 @@
+//
+//  mediaClubApp.swift
+//  mediaClub
+//
+//  Created by asmaa gamal  on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct mediaClubApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
